e86ad3347a15863a880943264bff2a1239b77768
Felix <feapta@gmail.com>
2023-11-02 19:44:30 +0100

    domo

--- views/domo.php
+++ views/domo.php
@@ -133,83 +133,98 @@
 
             <!-- Puerta verja -->
             <div class="puerta">
-                <div class="abrir_puerta">
-                    <button type="button" class="boton"  id="paso" onclick="puerta(2)">Puerta paso</button>
-                    <button type="button" class="boton"  id="vehiculos" onclick="puerta(1)">Puerta vehiculos</button>
-                </div>
-                <div class="cerrar">
-                    <button type="button" class="boton"  id="cierro" onclick="puerta(3)">Cerrar</button>
-                </div>
+                <button type="button" class="boton_verde btn_desplegable titulo_puerta"> <img class="img" src="/build/img/puertas.png" alt=""> Puertas</button>
+                <div class="desple">
+                    <div class="contenedor_puerta">
+                        <div class="abrir_puerta">
+                            <button type="button" class="boton_verde"  id="paso" onclick="puerta(2)">Puerta paso</button>
+                            <button type="button" class="boton_verde"  id="vehiculos" onclick="puerta(1)">Puerta vehiculos</button>
+                        </div>
+                        <div class="cerrar">
+                            <button type="button" class="boton_rojo"  id="cierro" onclick="puerta(3)">Cerrar</button>
+                        </div>
 
-                <div class="foco">
-                    <h5>Foco puerta</h5>
+                        <div class="foco">
+                            <h5>Foco puerta</h5>
 
-                <div class="botones_foco">
-                    <button type="button" class="boton" id="foco_on" onclick="foco(1)">On</button>
-                    <button type="button" class="boton" id="foco_off" onclick="foco(0)">Off</button>
+                            <div class="botones_foco">
+                                <button type="button" class="boton_verde" id="foco_on" onclick="foco(1)">On</button>
+                                <button type="button" class="boton_verde" id="foco_off" onclick="foco(0)">Off</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <!-- Mando del taller -->
             <div class="mando">
-                <div class="inversor">
-                    <h5>Inversor</h5>
-                    <div class="botones_inversor">
-                        <button type="button" class="boton " id="inversor_on" onclick="inversor(1)">On</button>
-                        <button type="button" class="boton " id="inversor_on" onclick="inversor(0)">On</button>
-                    </div>
-                </div>
+                <button type="button" class="boton_verde btn_desplegable titulo_taller"> <img class="img" src="/build/img/puertas.png" alt=""> Mando taller</button>
+                <div class="desple">
+                    <div class="contenedor_mando">
+                        <div class="inversor">
+                            <h5>Inversor</h5>
+                            <div class="botones_inversor">
+                                <button type="button" class="boton_verde" id="inversor_on" onclick="inversor(1)">On</button>
+                                <button type="button" class="boton_verde" id="inversor_on" onclick="inversor(0)">On</button>
+                            </div>
+                        </div>
 
-                <div class="termo">
-                    <h5>Termo</h5>
-                    <div class="botones_termo">
-                        <button type="button" class="boton " id="termo_on" onclick="termo(1)">On</button>
-                        <button type="button" class="boton " id="termo_off" onclick="termo(0)">Off</button>
+                        <div class="termo">
+                            <h5>Termo</h5>
+                            <div class="botones_termo">
+                                <button type="button" class="boton_verde" id="termo_on" onclick="termo(1)">On</button>
+                                <button type="button" class="boton_verde" id="termo_off" onclick="termo(0)">Off</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <!-- Motor -->
             <div class="motor">
-                <div class="contenedor_arriba">
-                    <div class="litros">
-                        <h5>Litros</h5>
-                        <p id="litros_gasoil"></p><p id="l">L</p>
-                    </div>
-                    <img  id="img_motor" src="/build/img/motor.png" alt="">
-                    <div class="deposito">
-                        <h5>Deposito</h5>
-                        <div>
-                            <div id="tanque_4"></div>
+                <div class="desple">
+                    <div class="contenedor_motor">
+                        <div class="contenedor_arriba">
+                            <div class="litros">
+                                <h5>Litros</h5>
+                                <p id="litros_gasoil"></p><p id="l">L</p>
+                            </div>
+                            <img  id="img_motor" src="/build/img/motor.png" alt="">
+                            <div class="deposito">
+                                <h5>Deposito</h5>
+                                <div>
+                                    <div id="tanque_4"></div>
+                                </div>
+                                <div>
+                                    <div id="tanque_3"></div>
+                                </div>
+                                <div>
+                                    <div id="tanque_2"></div>
+                                </div>
+                                <div>
+                                    <div id="tanque_1"></div>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <div id="tanque_3"></div>
+                        <div class="contenedor_boton">
+                            <button class="boton_verde" id="btn_motor" type="button" onclick="motor()"></button>
                         </div>
-                        <div>
-                            <div id="tanque_2"></div>
-                        </div>
-                        <div>
-                            <div id="tanque_1"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="contenedor_boton">
-                    <button class="boton" id="btn_motor" type="button" onclick="motor()"></button>
-                </div>
-                <div class="contenedor_contadores">
-                    <h5>Contadores</h5>
-                    <div class="contadores">
-                        <div class="totalHoras">
-                            <h5>Total</h5>
-                            <div id="total_horas" class="total"></div>
-                        </div>
-                        <div class="totalMes">
-                            <h5>Horas mes</h5>
-                            <div id="total_mes" class="mes"></div>
-                        </div>
-                        <div class="totalAhora">
-                            <h5>Ahora</h5>
-                            <div id="total_ahora" class="ahora"></div>
+                        <div class="contenedor_contadores">
+                            <h5>Contadores</h5>
+                            <div class="contadores">
+                                <div class="totalHoras">
+                                    <h5>Total</h5>
+                                    <div id="total_horas" class="total"></div>
+                                </div>
+                                <div class="totalMes">
+                                    <h5>Horas mes</h5>
+                                    <div id="total_mes" class="mes"></div>
+                                </div>
+                                <div class="totalAhora">
+                                    <h5>Ahora</h5>
+                                    <div id="total_ahora" class="ahora"></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -217,36 +232,41 @@
 
             <!-- iluminacion -->
             <div class="ilu">
-                <div class="contenedor_arriba">
-                    <div class="lumenes">
-                        <h5>Lumenes</h5>
-                        <p id="lux" ></p>
-                        <p>Lux</p>
+                <div class="desple">
+                    <div class="contenedor_ilu">
+                        <div class="contenedor_arriba">
+                            <div class="lumenes">
+                                <h5>Lumenes</h5>
+                                <p id="lux" ></p>
+                                <p>Lux</p>
+                            </div>
+                            <img  id="bombilla" src="/build/img/bombilla.jpg" alt="">
+                            <div class="vacio">  </div>
+                        </div>
+                        <div class="contenedor_botones" role="group" aria-label="Basic outlined example">
+                            <button id="btn_on"   class="boton_verde" type="button" onclick="ilu_envio(1)">On</button>
+                            <button id="btn_auto" class="boton_verde" type="button" onclick="ilu_envio(2)">Auto</button>
+                            <button id="btn_off"  class="boton_verde" type="button" onclick="ilu_envio(0)">Off</button>
+                        </div>
                     </div>
-                    <img  id="bombilla" src="/build/img/bombilla.jpg" alt="">
-                    <div class="vacio">  </div>
-                </div>
-                <div class="contenedor_botones" role="group" aria-label="Basic outlined example">
-                    <button id="btn_on"   type="button" onclick="ilu_envio(1)" class="boton">On</button>
-                    <button id="btn_auto" type="button" onclick="ilu_envio(2)" class="boton">Auto</button>
-                    <button id="btn_off"  type="button" onclick="ilu_envio(0)" class="boton">Off</button>
                 </div>
             </div>
 
 
             <!-- Meteo -->
+            <div class="contenedor_meteo"></div>
             <?php include_once __DIR__ . "/meteo.php";  ?>
 
         </div>
 
 
 
-        <?php $script = '<script src="/build/js/jquery-3-7-1.js"></script>'; ?>
-        <?php $script .= '<script type="module" src="/build/js/script.js"></script>'; ?>
+       
+        <?php $script .= '<script src="/build/js/script.js"></script>'; ?>
         <?php $script .= '<script src="/build/js/agua.js"></script>'; ?>
         <?php $script .= '<script src="/build/js/gasoil.js"></script>'; ?>
         <?php $script .= '<script src="/build/js/tension24V.js"></script>'; ?>
         <?php $script .= '<script src="/build/js/tension48V.js"></script>'; ?>
         <?php $script .= '<script src="/build/js/clima.js"></script>'; ?>
         <?php $script .= '<script src="/build/js/relog.js"></script>'; ?>
-        <?php $script .= '<script src="/build/js/mqtt.js"></script>'; ?>
+        <?php $script .= '<script src="/build/js/desplegable.js"></script>'; ?>
