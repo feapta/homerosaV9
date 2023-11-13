e9b7db07ceaa2a573cb850e01af6dd7f4be18f55
felixm <feapta@gmail.com>
2023-11-13 16:14:01 +0100

    horario

--- views/mando.php
+++ views/mando.php
@@ -9,7 +9,7 @@
                 <h5>Inversor</h5>
                 <div class="botones_inversor">
                     <button type="button" class="btn_pulsado pulsacion" id="inversor_on" onclick="inversor(1)">On</button>
-                    <button type="button" class="btn_pulsado pulsacion" id="inversor_on" onclick="inversor(0)">On</button>
+                    <button type="button" class="btn_pulsado pulsacion" id="inversor_off" onclick="inversor(0)">Off</button>
                 </div>
             </div>
 
