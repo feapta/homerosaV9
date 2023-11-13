eb81822a5834490cc4586f4073740f2ee3795164
felixm <feapta@gmail.com>
2023-11-13 14:42:42 +0100

    mando

--- views/mando.php
+++ views/mando.php
@@ -8,16 +8,16 @@
             <div class="inversor">
                 <h5>Inversor</h5>
                 <div class="botones_inversor">
-                    <button type="button" class="btn_pulsado" id="inversor_on" onclick="inversor(1)">On</button>
-                    <button type="button" class="btn_pulsado" id="inversor_on" onclick="inversor(0)">On</button>
+                    <button type="button" class="btn_pulsado pulsacion" id="inversor_on" onclick="inversor(1)">On</button>
+                    <button type="button" class="btn_pulsado pulsacion" id="inversor_on" onclick="inversor(0)">On</button>
                 </div>
             </div>
 
             <div class="termo">
                 <h5>Termo</h5>
                 <div class="botones_termo">
-                    <button type="button" class="btn_pulsado" id="termo_on" onclick="termo(1)">On</button>
-                    <button type="button" class="btn_pulsado" id="termo_off" onclick="termo(0)">Off</button>
+                    <button type="button" class="btn_pulsado pulsacion" id="termo_on" onclick="termo(1)">On</button>
+                    <button type="button" class="btn_pulsado pulsacion" id="termo_off" onclick="termo(0)">Off</button>
                 </div>
             </div>
         </div>
