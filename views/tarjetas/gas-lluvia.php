61a93951923659a53e75c29a134e55a28d6df3fd
felixm <feapta@gmail.com>
2024-01-08 20:28:21 +0100

    nivles

---
+++ views/tarjetas/gas-lluvia.php
@@ -0,0 +1,27 @@
+
+  <!-- Gas -->
+  <div class="gases sombraTarjetas">
+        <div class="tit">
+            <h5>Nivel de gas</h5>
+        </div>
+        <div class="conte">
+            <div class="parrafo">
+                <p class= "nivel_gas"></p> 
+            </div>
+        </div>
+    </div>
+
+    <!-- Lluvia -->
+    <div class="Lluvias sombraTarjetas">
+        <div class="tit">
+            <h5>Nivel de lluvia</h5>
+        </div>
+        <div class="conte">
+            <div class="conte_info">
+                <div class="parrafo">
+                    <p class= "lluvia"></p> 
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
