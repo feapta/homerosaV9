af3efd6372031f54e4572bd49134fce90643fd4b
felixm <feapta@gmail.com>
2024-04-30 20:21:07 +0200

    Botones

--- views/mando.php
+++ views/mando.php
@@ -9,6 +9,7 @@
                 <h5>Inversor</h5>
                 <div class="botones_inversor">
                     <button type="button" class="btn_pulsado pulsacion" id="inversor_on" onclick="inversor(1)">On</button>
+                    <button type="button" class="btn_pulsado pulsacion" id="inversor_auto" onclick="inversor(2)">Auto</button>
                     <button type="button" class="btn_pulsado pulsacion" id="inversor_off" onclick="inversor(0)">Off</button>
                 </div>
             </div>
@@ -17,6 +18,7 @@
                 <h5>Termo</h5>
                 <div class="botones_termo">
                     <button type="button" class="btn_pulsado pulsacion" id="termo_on" onclick="termo(1)">On</button>
+                    <button type="button" class="btn_pulsado pulsacion" id="termo_auto" onclick="termo(2)">Auto</button>
                     <button type="button" class="btn_pulsado pulsacion" id="termo_off" onclick="termo(0)">Off</button>
                 </div>
             </div>
