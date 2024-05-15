4a04c0014748ddefa23472e7c92ec35e1ee4409a
felixm <feapta@gmail.com>
2024-05-15 14:31:39 +0200

    pagina truck

--- views/truck/truck.php
+++ views/truck/truck.php
@@ -7,13 +7,16 @@
 <div class="contenedor_imagenes">
     <a href="/novedades">
         <img src="/src/img/truck/novedades.jpg" alt="">
+        Novedades
     </a>
 
     <a href="/productos">
         <img src="/src/img/truck/productos.webp" alt="">
+        Productos
     </a>
 
     <a href="/pruebas">
         <img src="/src//img/truck/pruebas.jpg" alt="">
+        Pruebas
     </a>
 </div>
