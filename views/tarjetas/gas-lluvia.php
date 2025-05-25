de432f30cd9d6cc2f0e734ccc3d1a2c5a3a5cacf
felixm <feapta@gmail.com>
2025-05-25 19:54:30 +0200

    lluvia

--- views/tarjetas/gas-lluvia.php
+++ views/tarjetas/gas-lluvia.php
@@ -2,27 +2,21 @@
     <!-- Gas -->
     <div class="gas sombraTarjetas">
         <div class="tit">
-            <h5>Nivel de gas</h5>
+            <h5>Alarma de gas</h5>
         </div>
-        <div class="cuadros">
-            <p class="gases">Gas</p>
-            <p class="limpio">Limpio</p>
-        </div>
+
         <div class="alarma_gas">
-            <h3 class="alarmaGas">Sin alarma</h3>
+            <h4 class="alarmaGas"></h4>
         </div>
     </div>
 
     <!-- Lluvia -->
     <div class="lluvias sombraTarjetas">
         <div class="tit">
-            <h5>Nivel de lluvia</h5>
+            <h5>Alarma de lluvia</h5>
         </div>
-        <div class="cuadros">
-            <p class="seco">Seco</p>
-            <p class="lluvia">Lluvia</p>
-        </div>
+
         <div class="alarma_lluvia" >
-            <h3 class="alamaLluvia">Alarma lluvia</h3>
+            <h4 class="alamaLluvia"></h4>
         </div>
     </div>
