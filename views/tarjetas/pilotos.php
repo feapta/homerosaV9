9d53c8dac51f29a259a3f884fb5b25158e06db99
felixm <feapta@gmail.com>
2025-06-03 19:48:05 +0200

    pilotos

--- views/tarjetas/pilotos.php
+++ views/tarjetas/pilotos.php
@@ -1,30 +1,30 @@
 
 
     <!-- Pilotos de estado-->
-    <div class="conexion">
-        <div class="conte">
-            <div class="pilotopiscina">
-                <p>PI</p>
-            </div>
-            <div class="pilotopuerta">
-                <p>PU</p>
-            </div>
-            <div class="pilotosalon">
-                <p>SA</p>
-            </div>
-            <div class="pilototaller">
-                <p>TA</p>
-            </div>
-            <div class="pilototinaja">
-                <p>TI</p>
-            </div>
+<div class="conexion">
+    <div class="conte">
+        <div class="pilotopiscina">
+            <p>PI</p>
         </div>
-        
-        <div class="radiacion_hoy">
-            <div class="conte">
-                <div class="indice_iv">
-                    <p class="in_iv cuadrado">indice iv</p>
-                </div>
-            </div>
+        <div class="pilotopuerta">
+            <p>PU</p>
+        </div>
+        <div class="pilotosalon">
+            <p>SA</p>
+        </div>
+        <div class="pilototaller">
+            <p>TA</p>
+        </div>
+        <div class="pilototinaja">
+            <p>TI</p>
         </div>
     </div>
+</div>
+
+<div class="radiacion_hoy">
+    <div class="conte">
+        <div class="indice_iv">
+            <p class="in_iv cuadrado">indice iv</p>
+        </div>
+    </div>
+</div>
