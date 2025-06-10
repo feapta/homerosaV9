30eab83836af677146f80f2c0f89c7cfdd49098b
felixm <feapta@gmail.com>
2025-06-10 13:37:28 +0200

    graficas

---
+++ views/paginas/graficas.php
@@ -0,0 +1,12 @@
+   <button type="button" class="boton_verde"> 
+        <img class="img" src="/build/img/graficas.png" alt="">
+        <h4>Graficas</h4>
+    </button>
+
+    <div class="desple">
+        <div class="contenedor_gra">
+
+  
+
+        </div>
+    </div>
