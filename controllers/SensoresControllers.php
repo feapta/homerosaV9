936e8fc94277159a57fc51efecd478f03fe43c6f
felixm <feapta@gmail.com>
2025-06-20 13:47:44 +0200

    graficas 21

--- controllers/SensoresControllers.php
+++ controllers/SensoresControllers.php
@@ -10,13 +10,11 @@ use Model\Sensores;
 class SensoresControllers {    
 
         public static function listar(){
-          $peticion = $_POST;
-
-           $diaCC = date ( 'j');
+            $diaCC = date ( 'j');
             $mesCC = date ( 'm');
             $yeaCC = date ( 'Y');
               
-            $consulta = " SELECT te, te_in, h, hu_su FROM medidas";
+            $consulta = " SELECT te, te_in, hu, hu_su FROM medidas";
             $consulta .= " WHERE d = $diaCC AND m= $mesCC AND y = $yeaCC";
 
             $respuesta = Sensores::SQL($consulta);
@@ -28,21 +26,6 @@ class SensoresControllers {
   }
   
   
-  // Grafica completa dia en curso
-    function dia_curso(){
-      debuguear("Ha llegado la peticion de consulta");
-      
-      $diaCC = date ( 'j');
-      $mesCC = date ( 'm');
-      $yeaCC = date ( 'Y');
-        
-      $consulta = " SELECT te, te_in, h, hu_su FROM medidas";
-      $consulta .= " WHERE d = $diaCC AND m= $mesCC AND y = $yeaCC";
-
-      $respuesta = Sensores::SQL($consulta);
-
-      $jsonstring_CC = json_encode($respuesta);
-      echo $jsonstring_CC;
-    }
+ 
 ?>
 
