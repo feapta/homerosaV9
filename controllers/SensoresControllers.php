f755b2aa9ad2c774ae46f687e151253dde4a1584
felixm <feapta@gmail.com>
2025-06-20 13:41:44 +0200

    consulta

--- controllers/SensoresControllers.php
+++ controllers/SensoresControllers.php
@@ -10,12 +10,20 @@ use Model\Sensores;
 class SensoresControllers {    
 
         public static function listar(){
-          debuguear($_POST);
-          $peticion = $_POST();
-         
-          if($peticion == 1){dia_curso();}
+          $peticion = $_POST;
 
-            
+           $diaCC = date ( 'j');
+            $mesCC = date ( 'm');
+            $yeaCC = date ( 'Y');
+              
+            $consulta = " SELECT te, te_in, h, hu_su FROM medidas";
+            $consulta .= " WHERE d = $diaCC AND m= $mesCC AND y = $yeaCC";
+
+            $respuesta = Sensores::SQL($consulta);
+
+            $jsonstring_CC = json_encode($respuesta);
+            echo $jsonstring_CC;
+      
     }
   }
   
