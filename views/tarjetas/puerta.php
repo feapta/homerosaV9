6b7ad9300d68843607d8251b08eb18eae5fdce4b
felixm <feapta@gmail.com>
2025-06-22 15:00:01 +0200

    puertas

--- views/tarjetas/puerta.php
+++ views/tarjetas/puerta.php
@@ -2,7 +2,7 @@
    
  <div class="puertas sombraTarjetas">
     <div class="conte">
-       <div class="abrir_puerta">
+       <div class="botones_puerta">
             <button type="button" class="btn_pulsado"  id="paso" onclick="puerta(1)">Puerta paso</button>
             <button type="button" class="btn_pulsado"  id="vehiculos" onclick="puerta(2)">Puerta vehiculos</button>
         </div>
