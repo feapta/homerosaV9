2e59f5d2e37554356b2c3cc61e060163ffd3d00e
felixm <feapta@gmail.com>
2025-06-22 15:11:08 +0200

    sigo tamaño

--- views/tarjetas/puerta.php
+++ views/tarjetas/puerta.php
@@ -2,10 +2,8 @@
    
  <div class="puertas sombraTarjetas">
     <div class="conte">
-       <div class="botones_puerta">
-            <button type="button" class="btn_pulsado"  id="paso" onclick="puerta(1)">Puerta paso</button>
-            <button type="button" class="btn_pulsado"  id="vehiculos" onclick="puerta(2)">Puerta vehiculos</button>
-        </div>
+        <button type="button" class="btn_pulsado"  id="paso" onclick="puerta(1)">Puerta paso</button>
+        <button type="button" class="btn_pulsado"  id="vehiculos" onclick="puerta(2)">Puerta vehiculos</button>
     </div>
 </div>
    
