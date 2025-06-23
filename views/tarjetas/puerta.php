3a6c59f863c22a6f2fe7bc10a78397caa4e8ebe7
felixm <feapta@gmail.com>
2025-06-23 13:27:41 +0200

    colores

--- views/tarjetas/puerta.php
+++ views/tarjetas/puerta.php
@@ -2,8 +2,8 @@
    
  <div class="puertas sombraTarjetas">
     <div class="conte">
-        <button type="button" class="btn_pulsado"  id="paso" onclick="puerta(1)">Puerta paso</button>
-        <button type="button" class="btn_pulsado"  id="vehiculos" onclick="puerta(2)">Puerta vehiculos</button>
+        <button type="button" class="btn_pulsado_P"  id="paso" onclick="puerta(1)">Puerta paso</button>
+        <button type="button" class="btn_pulsado_P"  id="vehiculos" onclick="puerta(2)">Puerta vehiculos</button>
     </div>
 </div>
    
