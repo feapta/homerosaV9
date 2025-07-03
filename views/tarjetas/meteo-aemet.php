1b90961e1b6272b88d659c030f6ee2021a4a6690
felixm <feapta@gmail.com>
2025-07-03 20:45:12 +0200

    color

--- views/tarjetas/meteo-aemet.php
+++ views/tarjetas/meteo-aemet.php
@@ -1,6 +1,6 @@
     <!-- Meto aemet -->
     <div class="aemet sombraTarjetas">
         <div class="conte">
-            <iframe name="iframe_aemet_id33044" width="100%" height="100%" tabindex="0" id="iframe_aemet_id33044" src="https://www.aemet.es/es/eltiempo/prediccion/municipios/mostrarwidget/malagon-id13052?w=g4p011100110ohmffffffx00cc99t00cc99r1s3n1" frameborder="0" scrolling="no"></iframe>
+            <iframe name="iframe_aemet_id33044" width="100%" height="100%" tabindex="0" id="iframe_aemet_id33044" src="https://www.aemet.es/es/eltiempo/prediccion/municipios/mostrarwidget/malagon-id13052?w=g4p011100110ohmffffffx33cccct33ccccr1s3n1" frameborder="0" scrolling="no"></iframe>
         </div>
     </div>
